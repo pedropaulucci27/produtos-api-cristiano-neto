7824b3eab3bd7f0e4cebb0ee739aa35494b97eb8
Pedro Paulucci <peeronoronha@gmail.com>
2026-03-12 08:54:03 -0300

    feat(produtos): cria router e registra rotas no app.js

---
+++ src/routes/produtos.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const produtosController = require("../controllers/produtosController");
+
+router.get("/", produtosController.listar);
+router.get("/:id", produtosController.buscarPorId);
+router.post("/", produtosController.criar);
+router.put("/:id", produtosController.atualizar);
+router.delete("/:id", produtosController.remover);
+
+module.exports = router;
